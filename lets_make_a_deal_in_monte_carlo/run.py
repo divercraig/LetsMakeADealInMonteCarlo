b646d9ffd465a7b0dcbd88ddc5c237c0efbcbe42
Craig Warren <craig@craigwarren.co.uk>
2019-11-24 17:17:23 +0000

    refactoring the run package to remove duplicate code

--- lets_make_a_deal_in_monte_carlo/run.py
+++ lets_make_a_deal_in_monte_carlo/run.py
@@ -2,38 +2,42 @@ import logging
 from lets_make_a_deal_in_monte_carlo.lets_make_a_deal import LetsMakeADealGame
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+
+def run_a_game(game_number: int, switch: bool):
+    win = False
+    game = LetsMakeADealGame(switch=switch)
+    if game.play() is True:
+        logging.debug("Game %i was a winner", game_number)
+        win = True
+    else:
+        logging.debug("Game %i was a loser", game_number)
+    logging.debug("+++++++++++++++++++++++++++++")
+    return win
+
+
+def run_a_simulation(num_games: int, switch: bool):
+    wins = 0
+    strategy_name = "SWITCH" if switch is True else "STICK"
+    logging.info("RUNNING %i GAMES WITH THE %s STRATEGY", num_games, strategy_name)
+    for x in range(1, num_games + 1):
+        if run_a_game(switch=switch, game_number=x) is True:
+            wins += 1
+
+    logging.info("COMPLETED %i GAMES WITH THE %s STRATEGY- %i WON", num_games, strategy_name, wins)
+    logging.info("-----------------------------------------------")
+
+    return wins / num_games * 100
+
+
 sample_size = 100000
-stick_wins = 0
-switch_wins = 0
 
-logging.info("RUNNING %i STICK SIMULATIONS", sample_size)
-for x in range(1, sample_size + 1):
-    game = LetsMakeADealGame()
-    if game.play() is True:
-        logging.debug("Game %i was a winner", x)
-        stick_wins += 1
-    else:
-        logging.debug("Game %i was a loser", x)
-    logging.debug("+++++++++++++++++++++++++++++")
-
-logging.info("COMPLETED %i STICK SIMULATIONS - %i WON", sample_size, stick_wins)
-
-logging.info("-----------------------------------------------")
-
-logging.info("RUNNING %i SWITCH SIMULATIONS", sample_size)
-for x in range(1, sample_size + 1):
-    game = LetsMakeADealGame(switch=True)
-    if game.play() is True:
-        logging.debug("Game %i was a winner", x)
-        switch_wins += 1
-    else:
-        logging.debug("Game %i was a loser", x)
-    logging.debug("+++++++++++++++++++++++++++++")
-
-logging.info("COMPLETED %i SWITCH SIMULATIONS - %i WON", sample_size, switch_wins)
-
-logging.info("-----------------------------------------------")
+stick_success_rate = run_a_simulation(num_games=sample_size, switch=False)
+switch_success_rate = run_a_simulation(num_games=sample_size, switch=True)
 
 logging.info("SIMULATIONS COMPLETE")
-logging.info("Stick won %d percent of the time", stick_wins/sample_size * 100)
-logging.info("Switch won %d percent of the time", switch_wins/sample_size * 100)
+logging.info("Stick won %d percent of the time", stick_success_rate)
+logging.info("Switch won %d percent of the time", switch_success_rate)
+
+
+
